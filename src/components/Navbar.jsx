87eece70048cb2da149f3b5b01c72c6a5d635419
orion <munna.csehstu@gmail.com>
2024-11-04 01:39:29 +0600

    changed navbar color and need more work to complete it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 
 const Navbar = () => {
+    const location = useLocation();
     return (
-        <div className="navbar bg-base-100 max-w-screen-xl mx-auto px-0">
+        <div className={`navbar  max-w-screen-xl mx-auto px-0 ${location.pathname === "/" ? "bg-[rgb(149,56,226)]" : "bg-white"}`}>
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div tabIndex={0} role="button" className={`btn btn-ghost lg:hidden`}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
@@ -22,20 +23,44 @@ const Navbar = () => {
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        className={`menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow `}>
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/statistics">Statistics</NavLink></li>
                         <li><NavLink to="/dashboard">Dashboard</NavLink></li>
 
                     </ul>
                 </div>
-                <Link className=" text-xl font-bold">Gadget Heaven</Link>
+                <Link className={`text-xl font-bold  ${location.pathname === "/" ? " text-white" : ""}`}>Gadget Heaven</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 gap-3">
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/statistics">Statistics</NavLink></li>
-                    <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+                <ul className={`menu menu-horizontal px-1 gap-3 ${location.pathname === "/" ? " text-white" : ""}`}>
+                    <li>
+                        <NavLink
+                            to="/"
+                            // className={({ isActive }) =>isActive ? "border-none border-b-2 text-green-400 text-3xl" : ""}
+                            className={`${location.pathname === "/" ? " text-white" : ""}`}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li><NavLink to="/statistics"
+                        // className={({ isActive }) =>{
+                        //      isActive ? "border-b-2" : "";
+                        //      location.pathname==="/"? "border-white":""
+                        //     }
+                        // }
+                        // className={({ isActive }) =>isActive ? "border-none border-b-2 border-red-500 text-green-400 text-3xl" : ""}
+                        className={`${location.pathname === "/" ? " text-white" : ""}`}
+                    >Statistics</NavLink></li>
+                    <li><NavLink to="/dashboard"
+                        // className={({ isActive }) =>{
+                        //      isActive ? "border-b-2" : "";
+                        //      location.pathname==="/"? "border-white":""
+                        //     }
+                        // }
+                        // className={({ isActive }) =>isActive ? "border-none border-b-2 text-green-400 text-3xl" : ""}
+                        className={`${location.pathname === "/" ? " text-white" : ""}`}
+                    >Dashboard</NavLink></li>
                 </ul>
             </div>
             <div className="navbar-end gap-3">
