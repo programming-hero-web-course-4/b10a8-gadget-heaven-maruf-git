708471ce64b9eb9b84c2a116164097aa2689ea04
orion <munna.csehstu@gmail.com>
2024-11-05 00:22:25 +0600

    added cart menubar

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,18 @@
 // import React from 'react';
+import { GiSettingsKnobs } from "react-icons/gi";
+
 
 const Cart = () => {
     return (
-        <div className="my-10">
-            cart
+        <div className="my-5">
+            <div className="flex justify-between items-center py-5">
+                <h1 className="font-bold text-2xl">Cart</h1>
+                <div className="flex gap-5 items-center">
+                    <h1 className="font-bold text-2xl" >Total Cost: {0} $</h1>
+                    <button className="btn w-[150px] rounded-full bg-white border-[rgb(149,56,226)] text-[rgb(149,56,226)]">Sort by Price <GiSettingsKnobs className="font-bold text-xl" /></button>
+                    <button className="btn rounded-full w-[150px] bg-[rgb(149,56,226)] text-white">Purchase</button>
+                </div>
+            </div>
         </div>
     );
 };
